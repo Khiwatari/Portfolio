403a7d0b98b46a42d8a2b219232c7655bfa75d79
Gagan <gagan270903@gmail.com>
2022-06-05 14:19:46 +0530

    second commit

---
+++ JS/index.js
@@ -0,0 +1,18 @@
+let skillContent = document.getElementsByClassName('skill_content')
+let skillHead = document.getElementsByClassName('skill_header')
+
+
+skillHead.addEventListener('click')
+
+// function toggleSkill(){
+//     let item = this.parentnode.className 
+
+//     for(let i = 0; i<skillContent.length; i++ ){
+//         skillContent[i].className = 'skill_content skill_close'
+//     }
+//     if (item == 'skill_content skill_close') {
+//         this.parentnode.className = "skill_content skill_open"
+//     }
+// }
+
+// skillHead.addEventListener('click',toggleSkill)
