859db728763a0a10f54058e2a53601595d5c79d4
Gagan <gagan270903@gmail.com>
2022-06-05 15:22:10 +0530

    third

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,20 @@
-let skillContent = document.getElementsByClassName('skill_content')
-let skillHead = document.getElementsByClassName('skill_header')
+const skillContent = document.getElementsByClassName('skill_content')
+const skillHead = document.querySelectorAll('.skill_header')
 
+console.log(typeof(skillHead))
 
-skillHead.addEventListener('click')
+function toggleSkill() {
+    let item = this.parentNode.className
 
-// function toggleSkill(){
-//     let item = this.parentnode.className 
+    for (let i = 0; i < skillContent.length; i++) {
+        skillContent[i].className = 'skill_content skill_close'
+    }
 
-//     for(let i = 0; i<skillContent.length; i++ ){
-//         skillContent[i].className = 'skill_content skill_close'
-//     }
-//     if (item == 'skill_content skill_close') {
-//         this.parentnode.className = "skill_content skill_open"
-//     }
-// }
+    if (item === 'skill_content skill_close') {
+        this.parentNode.className = "skill_content skill_open"
+    }
+}
 
-// skillHead.addEventListener('click',toggleSkill)
+skillHead.forEach((element) =>{
+    element.addEventListener('click',toggleSkill)
+})
